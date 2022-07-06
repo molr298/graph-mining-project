504d39302d2a793d09d3058189c183b263be0dab
Nhất Huy <62438532+yul1492101@users.noreply.github.com>
2022-07-06 14:46:10 +0700

    tmp

--- utils/algorithm1.py
+++ utils/algorithm1.py
@@ -1,46 +1,17 @@
 import igraph as ig
 import networkx as nx
-
+inf = float('inf')
 class PNSubgraph:
-    def __init__(self, N_vertex, Graph):
-        self.C_id = []
+    def __init__(self, N_vertex, thresh_PN):
+        self.C_id = [None]*N_vertex
         self.Size = 0
-        self.Level = []
-        self.PN = []
-        self.Count = []
+        self.Level = [inf]*N_vertex
+        self.PN = [inf]*thresh_PN
+        self.Count = [0]*N_vertex
 
-G = nx.full_rary_tree(3, 5)
+G = nx.graph()
 def BFS_extract_PN_subgraph(V, E, N_vertex):
-    thresh_PN = 6
-    C_id = [None] * N_vertex
-    PN = []
-    size = [0]*N_vertex
-    Q = []  
-    level = [-1]*len(V)
-    count = [0]*len(V)
-    for i in range(N_vertex):
-        start = 0
-        end = 1
-        Q.insert(0, V[i])
-        level[i] = 0
-        C_id[i] = V[i]
-        while start!=end:
-            stop = end
-            j = start
-            while j <= end:
-                for d in E[Q[j][0]]:
-                    if level[d]==-1:
-                        Q[end] = d
-                        end+=1
-                        level[d]=level[Q[j][0]]+1
-                        C_id[d]=C_id[Q[j][0]]
-                    if count[d] < thresh_PN:
-                        if level[Q[j][0]] < level[d]:
-                            PN[[d][count[d]]] = Q[j][0]
-                            count[d]+=1
-                        elif level[Q[j][0]] == level[d]:
-                            PN[[[d][count[d]]]] = -Q[j][0]
-                            count[d]+=1
-            start=stop
-        size[i]=end
+    thresh_PN = 2
+    res = PNSubgraph()
+
                         
